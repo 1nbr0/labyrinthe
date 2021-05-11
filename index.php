c017e4aea1f03f2d741bdd3823eea4064f8a91d2
Robin TURPIN <robin.turpin@campus.academy>
2021-05-11 16:04:06 +0200

    changes

--- index.php
+++ index.php
@@ -1,54 +1,85 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<HEAD>
+<head>
     <meta charset="utf-8" />
+    <title>Le Labyrinthe</title>
     <style>
         .mur {
-            background-color: green;
+            background-image: url("image/mur.jpg");
         }
 
         .chemin {
-            background-color: orange;
+            background-image: url("image/chemin.jpg");
         }
+
+        .sortie {
+            background-image: url("image/sortie.jpg");
+        }
+        .perso {
+            background-image: url("image/perso.jpg");
+        }
+
         td {
-            width: 20px;
-            height: 20px;
+            width: 50px;
+            height: 50px;
         }
     </style>
 
-</HEAD>
+</head>
 
 <body>
+    <h1>Bonjour, bienvenue dans le jeu du Labyrinthe.</h1>
+    <div>
+        <h2>Choix du pseudo :</h2>
+        <form action="" method="POST">
+            <input type="text" name="pseudo">
+            <button type="submit">Confirmer votre pseudo</button>
+        </form>
+    </div>
+    <div>
+        <button type="submit">Recommencer</button>
+    </div>
     <div>
         <?php
-            $labyrinthe = fopen('labyrinthe.txt', 'r+');
+        $labyrinthe = fopen('labyrinthe.txt', 'r+');
 
-            $tabLab = [];
-            while(!feof($labyrinthe)) {
-                $ligne = fgets($labyrinthe);
-                $tabLab[] = explode(" ", $ligne);
-                echo $ligne . "<br />";
-            }
-            // var_dump($tabLab);
+        $tabLab = [];
+        while (!feof($labyrinthe)) {
+            $ligne = fgets($labyrinthe);
+            $tabLab[] = explode(" ", $ligne);
+        }
+        // var_dump($tabLab);
 
-            fclose($labyrinthe);
+        fclose($labyrinthe);
         ?>
         <table>
-            <?php foreach( $tabLab as $ligne) :?>
+            <?php foreach ($tabLab as $ligne) : ?>
                 <tr>
-                    <?php foreach( $ligne as $case) :?>
-                        <?php if($case == 0) :?>
+                    <?php foreach ($ligne as $case) : ?>
+                        <?php if ($case == 0) : ?>
                             <td class="mur">
                         <?php endif; ?>
-                        <?php if($case == 1) :?>
+                        <?php if ($case == 1) : ?>
                             <td class="chemin">
                         <?php endif; ?>
-                        </td>
+                        <?php if ($case == 2) : ?>
+                            <td class="sortie">
+                        <?php endif; ?>
+                        <?php if ($case == 3) : ?>
+                            <td class="perso">
+                        <?php endif; ?>
+                            </td>
                     <?php endforeach; ?>
                 </tr>
             <?php endforeach; ?>
         </table>
+    </div>
+    <div>
+        <button type="submit">Haut</button>
+        <button type="submit">Bas</button>
+        <button type="submit">Gauche</button>
+        <button type="submit">Droite</button>
     </div>
 </body>
 
